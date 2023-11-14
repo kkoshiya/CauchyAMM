d2e6218ccbe9f3df9b7818031d207e7781318395
Kyle Koshiyama <43145199+kkoshiya@users.noreply.github.com>
2023-11-14 11:10:54 -0800

    first

---
+++ contracts/test.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.19;
+
+import { SD59x18 } from "@prb/math/SD59x18.sol";
+//import { PRBTest } from "@prb/math/test/PRBTest.sol";
+
+contract Test {
+
+    function _test() public {
+
+    }
+
+
+}
